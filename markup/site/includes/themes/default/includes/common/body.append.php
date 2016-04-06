e5d7a606e0c42f8a3c29a0a14c6a4ae00cafe061
Joshua Logsdon <logsdon.joshua@gmail.com>
2016-04-06 10:04:14 -0400

    Tweak includes and styles, fix finding view files that are default views inside directories

--- markup/site/includes/themes/default/includes/common/body.append.php
+++ markup/site/includes/themes/default/includes/common/body.append.php
@@ -1,22 +1,14 @@
 
 <!-- JavaScript -->
 
-<script type="text/javascript">
-//<![CDATA[
-jQuery(document).ready(function($)
-{
-});
-//]]>
-</script>
-
 <link rel="stylesheet" type="text/css" href="<?php echo redlove_cb_url('javascript/redlove/plugins/redlove_code_example.css'); ?>">
 <script type="text/javascript" src="<?php echo redlove_cb_url('javascript/redlove/plugins/redlove_code_example.js'); ?>"></script>
+
 <script type="text/javascript">
 //<![CDATA[
-	jQuery(document).ready(function($)
+	jQuery(document).ready(function ( $ )
 	{
 		$.fn.redlove_code_example();
 	});
 //]]>
 </script>
-
--- markup/site/index.php
+++ markup/site/index.php
@@ -9,26 +9,49 @@ if ( ! defined('ENVIRONMENT') )
 // You could take theme files, place them at the site root, and strip out the THEME* constant usage or update the site_settings with blank template info, so it looks at the root, and have a more simple site template structure.
 
 // Look for view file to display if not placing content directly on the page
+$view = false;
+
+// Check for existing view file to show
 $view_page = ( PAGE == '' ) ? 'index' : PAGE;
-$view_file = THEME_PATH . $view_page . '.php';
-if ( file_exists($view_file) )
+$view_path = THEME_PATH . $view_page;
+$view_file = $view_path . '.php';
+if ( is_file($view_file) && file_exists($view_file) )
 {
-	require_once($view_file);
+	$view = $view_file;
+}
+
+// Check if directory exists and index in directory
+if ( ! $view && file_exists($view_path) )
+{
+	$view_page .= '/index';
+	$view_path = THEME_PATH . $view_page;
+	$view_file = $view_path . '.php';
+	if ( is_file($view_file) && file_exists($view_file) )
+	{
+		$view = $view_file;
+	}
+}
+
+if ( $view )
+{
+	require_once($view);
 }
 else
 {
 	header('HTTP/1.0 404 Not Found', true, 404);
 	
 	$view_file = THEME_PATH . 'error_404.php';
-	if ( file_exists($view_file) )
+	if ( is_file($view_file) && file_exists($view_file) )
 	{
 		require_once($view_file);
 	}
 	else
 	{
 	?>
+	
 	<h1>404 Not Found</h1>
 	<p>We can't find what you are looking for.</p>
+	
 	<?php
 	}
 }
